078f095b0da21baafeec4f1c1317c2ffc9fd26b9
dong <z5105475@student.unsw.edu.au>
2020-01-16 22:47:13 +1100

    nothing

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
+
 // mongoose.connect("mongodb://localhost:27017/yelp_camp_v9", {useNewUrlParser: true})
+
 mongoose.connect("mongodb+srv://dongyun:dongyunPassword@cluster0-oi5ll.mongodb.net/test?retryWrites=true&w=majority"
 ).then(() => {
     console.log('Connected to DB!');
